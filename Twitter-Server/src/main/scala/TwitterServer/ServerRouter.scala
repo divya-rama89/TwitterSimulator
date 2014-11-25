deac24fba20765cbf3c79eee133f4f6c5a9fe4b6
divya-rama89 <divya.rama89@gmail.com>
2014-11-25 16:52:25 +0530

    Update ServerRouter.scala
    Shifted users/followers generation and storage to assigner; Replaced tweetID with tweet text.

--- Twitter-Server/src/main/scala/TwitterServer/ServerRouter.scala
+++ Twitter-Server/src/main/scala/TwitterServer/ServerRouter.scala
@@ -4,9 +4,7 @@ import akka.actor.ActorSystem
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.Map
-import scala.math.BigInt
-
-
+import scala.collection.mutable.HashMap
 
 case class tweet(tweetText : String)
 case class readStatus()
@@ -16,15 +14,16 @@ class ServerRouter(numUsers: Int, ac: ActorSystem)  extends Actor{
 
   val ServerAssignService:ArrayBuffer[ActorRef] = new ArrayBuffer[ActorRef]
   
-  // followerTable: userID, follower ID
-  var followerTable = scala.collection.mutable.HashMap.empty[Int,ListBuffer[Int]]
   // tweetTable : tweetID, text and owner's userID
-  var tweetTable = scala.collection.mutable.HashMap.empty[Int,Tuple2[Int,String]]
+  // var tweetTable = scala.collection.mutable.HashMap.empty[Int,Tuple2[Int,String]]
   var tweetIDCtr : Int = 0
- 
+  var initCtr : Int = 0
+  
   // constants
-  val NUMBEROFSERVERASSIGNERS = 4 
-  val TWEETLIMIT = Int.MaxValue 
+  val NUMBEROFSERVERASSIGNERS = 64 
+  val FOLLOWERSLIMIT = 10
+  val DEBUG = true
+  //val TWEETLIMIT = Int.MaxValue 
   
   // Will receive two types of messages from client 
   // Tweet message : senderNodeId. tweet
@@ -34,102 +33,87 @@ class ServerRouter(numUsers: Int, ac: ActorSystem)  extends Actor{
   
   def receive = {
     case "Init" => init() //generate actors + users/followers table
+    case "done" => incInitCtr()
     case tweet(tweetText: String) => routeTweetMessage(sender,tweetText)
     case readStatus() => routeStatusRequest(sender)
-    case sendMeTweets() => sendingTweets(sender)
+    //case sendMeTweets() => sendingTweets(sender)
   }
   
   
   def init() { 
-    var x:Int = 1
-    var y:Int = 0
-    var rnd = new scala.util.Random
     
-    for(x <- 1 to numUsers) {
-      // simulate twitter statistics for followers
-      // call function returnFollowers()
-      var quantity = rnd.nextInt(10)
-      var seq: ListBuffer[Int] = new ListBuffer
-      
-      for(y <- 0 to quantity) {
-        var randomNumber = rnd.nextInt(numUsers)
-        while((randomNumber < 0) || (seq.contains(randomNumber))) {
-        randomNumber = rnd.nextInt(numUsers)
-       }
-      seq += randomNumber
-      
-      }  
-      seq += x
-      
-      // DEBUG
-      /*
-      var z = 0
-     for(z<-0 to seq.length-1) {
-       println(seq(z))
-     } 
-      */
-      // fill up hash map
-      followerTable.put(x, seq)
-   }
-    
-       // DEBUG
-      val dummy = ac.actorOf(Props(new TwitterClient(ac)), "3")
-      println("path of dummy  is " + dummy.path)
-      dummy ! "test" 
-    
-    // Create 4 ServerAssigners
+    // Create NUMBEROFSERVERASSIGNERS ServerAssigners
     for(x <- 0 to NUMBEROFSERVERASSIGNERS-1) {
-    	ServerAssignService += ac.actorOf(Props(new ServerAssigner(followerTable, ac)), "ServerAssigner"+x)
+    	ServerAssignService += ac.actorOf(Props(new ServerAssigner(numUsers, ac, x, NUMBEROFSERVERASSIGNERS, FOLLOWERSLIMIT)), "ServerAssigner"+x)
     	println("path is " + ServerAssignService(x).path)		
+    }
+    
+    for(x <- 0 to NUMBEROFSERVERASSIGNERS-1) {
+      ServerAssignService(x) ! "init"	
+      ServerAssignService(x) ! AssignersListSend(ServerAssignService)
+    }
+    
+    if(DEBUG) {
+    	println("server init done")
     }
   }
   
+  def incInitCtr() {
+    var x = 0
+    initCtr += 1
+    if(initCtr == NUMBEROFSERVERASSIGNERS) {
+        if(DEBUG) {
+          println("All assigners initialised!")
+        }
+     
+      // safe to send test tweet  
+      if(DEBUG) {
+      val dummy = ac.actorOf(Props(new TwitterClient(ac)), "3")
+      println("path of dummy  is " + dummy.path)
+      dummy ! "test" 
+    }
+    }
+  }
   
   def routeTweetMessage(sender:ActorRef, tweetText:String) {
 	  // generate tweet ID, put onto tweet table and pass to worker
-    println(tweetText)
+    if(DEBUG) {
+    	println("tweet : " + tweetText)
+    }
     tweetIDCtr += 1
-    println("inside routeTweet "+ sender.path.toString().substring(26).toInt)
+    
+    if(DEBUG) {
+    	println("inside routeTweet of router..ownerOfTweet- "+ sender.path.toString().substring(26).toInt)
+    }
     
     val ownerID = (sender.path.toString).substring(26).toInt
-    var tweetInfo = new Tuple2(ownerID, tweetText) 
+   // var tweetInfo = new Tuple2(ownerID, tweetText) 
     
     // TODO: add a queue to make it fifo
-    if(tweetIDCtr < TWEETLIMIT) {
-    tweetTable.put(tweetIDCtr, tweetInfo)
-    }
+   
+    //tweetTable.put(tweetIDCtr, tweetInfo)
+   
+    ServerAssignService(ownerID % NUMBEROFSERVERASSIGNERS) ! tweetReceived(tweetText, ownerID)
   
-    followerTable(ownerID);
-    println(followerTable .mkString(","))
-    
-    var retrievedFollowers:ListBuffer[Int] = followerTable(ownerID)
-    println("retrievedFollowers for given ownerID "+ownerID)   
-    //retrievedFollowers.foreach { i =>println(i)
-      println(retrievedFollowers.mkString(","))
-    
-    var partitionedFollowers = retrievedFollowers.groupBy(x=>x % NUMBEROFSERVERASSIGNERS)
-    println("partitioned followers for given ownerID "+ownerID)
-      partitionedFollowers.foreach { i => println(i) }
-    
-    // Send the related assigner/worker
-    
-    for(x <- 0 to NUMBEROFSERVERASSIGNERS-1) {
-    	if(((partitionedFollowers.get(x % (NUMBEROFSERVERASSIGNERS))).size != 0)) {
-    		ServerAssignService(x % (NUMBEROFSERVERASSIGNERS)) !  updateTimeline(tweetIDCtr, ownerID, partitionedFollowers(x % (NUMBEROFSERVERASSIGNERS)))			
-    	}
-    }   
   }
 
   def routeStatusRequest(sender:ActorRef) {
     
     var senderID = sender.path.toString().substring(26).toInt 
-    
-    // check whether to send sender or senderID
-    ServerAssignService(senderID % NUMBEROFSERVERASSIGNERS) ! requestStatus(senderID)
+    /*
+    if(DEBUG){
+      println("sender="+sender)
+      println("sender ID="+senderID)
+      println("NUMBEROFSERVERASSIGNERS="+NUMBEROFSERVERASSIGNERS)
+      println("(senderID % NUMBEROFSERVERASSIGNERS)="+(senderID % NUMBEROFSERVERASSIGNERS))
+    }
+    * 
+    */
+   
+    ServerAssignService(senderID % NUMBEROFSERVERASSIGNERS) ! requestStatus(senderID, sender)
+    if(DEBUG){
+      println("sent")
+    }
   }
-  
-  def sendingTweets(sender: ActorRef) {
-    sender ! tweetTableSend(tweetTable)
-  }
-  
+
 }
