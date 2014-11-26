4e670c152c311c63d9d75c5748e2ec65cd253eed
divya-rama89 <divya.rama89@gmail.com>
2014-11-26 14:45:03 +0530

    Update Server.scala
    Post-test code

--- Twitter-Server/src/main/scala/TwitterServer/Server.scala
+++ Twitter-Server/src/main/scala/TwitterServer/Server.scala
@@ -1,27 +1,22 @@
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{ Props, ActorSystem }
 import com.typesafe.config.ConfigFactory
 
+object Server {
+  def main(args: Array[String]): Unit = {
 
-object ServerMain {
-def main(args: Array[String]): Unit = {
-     
-     val system = ActorSystem("TwitterServer")
+    val system = ActorSystem("TwitterServer")
 
-     var numUsers:Int = 0
-     var numCli:Int = 0
-     if(args.length > 1){
-    	 numUsers = (args(0).toInt)
-    	 numCli = args(1).toInt
-     } else {
-       println("Usage : Server.scala <Number of Users> <Number of Clients>")
-     }
-               
-     val ServerRouterService = system.actorOf(Props(new ServerRouter(numUsers, numCli, system)), "ServerRouter")
-     println("path is " + ServerRouterService.path)
-     ServerRouterService ! "Init"
-     ServerRouterService ! "test"
-  
-    
-     
-   }
+    var numUsers: Int = 0
+    var numCli: Int = 0
+    if (args.length > 1) {
+      numUsers = (args(0).toInt)
+      numCli = args(1).toInt
+
+      val ServerRouterService = system.actorOf(Props(new ServerRouter(numUsers, numCli, system)), "ServerRouter")
+      println("path is " + ServerRouterService.path)
+      ServerRouterService ! "Init"
+    } else {
+      println("Usage : Server.scala <Number of Users> <Number of Clients>")
+    }
+  }
 }
