46952322b8f5e64ba35328b960b1fdf6b0893ed3
divya-rama89 <divya.rama89@gmail.com>
2014-12-05 10:45:08 +0530

    Update Server.scala
    Added multiple ServerRouters. (Fixed number = 4; needs to be modified if it has to be configurable)

--- Twitter-Server/src/main/scala/TwitterServer/Server.scala
+++ Twitter-Server/src/main/scala/TwitterServer/Server.scala
@@ -11,10 +11,21 @@ object Server {
     if (args.length > 1) {
       numUsers = (args(0).toInt)
       numCli = args(1).toInt
-
+/*
       val ServerRouterService = system.actorOf(Props(new ServerRouter(numUsers, numCli, system)), "ServerRouter")
       println("path is " + ServerRouterService.path)
       ServerRouterService ! "Init"
+  */  
+    // create 4 server routers
+    // can create a macro and make this configurable
+      var i = 0
+      
+      for(i <- 0 to 3) {
+      var ServerRouterService = system.actorOf(Props(new ServerRouter(numUsers, numCli, system)), "ServerRouter"+i)
+      println("path is " + ServerRouterService.path)
+      if(i == 0) ServerRouterService ! "Init"
+    //  ServerRouterService ! "init2"
+      }
     } else {
       println("Usage : Server.scala <Number of Users> <Number of Clients>")
     }
